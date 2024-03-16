6df5a61591e3ed19717109dbe00146cbbf4c8dd2
Ginlon <2675130594@qq.com>
2024-03-16 13:45:44 +0800

    feat: zustand

--- zustand-test/src/App.js
+++ zustand-test/src/App.js
@@ -1,15 +1,49 @@
-import { create } from "zustand";
+import { create } from "./MyZustand";
+import { useEffect } from "react";
+import { persist } from "zustand/middleware";
 
-const useXxxStore = create((set) => ({
-  aaa: "",
-  bbb: "",
-  updateAaa: (value) => set(() => ({ aaa: value })),
-  updateBbb: (value) => set(() => ({ bbb: value })),
-}));
+// const useXxxStore = create(
+//   logMiddleware((set) => ({
+//     aaa: "",
+//     bbb: "",
+//     updateAaa: (value) => set(() => ({ aaa: value })),
+//     updateBbb: (value) => set(() => ({ bbb: value })),
+//   }))
+// );
+
+const useXxxStore = create(
+  logMiddleware(
+    persist(
+      (set) => ({
+        aaa: "",
+        bbb: "",
+        updateAaa: (value) => set(() => ({ aaa: value })),
+        updateBbb: (value) => set(() => ({ bbb: value })),
+      }),
+      { name: "xxx-store" }
+    )
+  )
+);
+function logMiddleware(func) {
+  return function (set, get, store) {
+    function newSet(...args) {
+      console.log("调用了", get());
+      return set(...args);
+    }
+
+    return func(newSet, get, store);
+  };
+}
 
 export default function App() {
   const updateAaa = useXxxStore((state) => state.updateAaa);
   const aaa = useXxxStore((state) => state.aaa);
+
+  // useEffect(() => {
+  //   useXxxStore.subscribe((state) => {
+  //     console.log(useXxxStore.getState());
+  //   });
+  // }, []);
 
   return (
     <div>
