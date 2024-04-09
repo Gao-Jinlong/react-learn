01930c80c7160c88f3fc30085316a887beeec31e
Ginlon <2675130594@qq.com>
2024-04-10 00:28:40 +0800

    feat:react-dnd

--- react-dnd-test/src/App2.tsx
+++ react-dnd-test/src/App2.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App2.css";
+import { useDrag, useDrop } from "react-dnd";
 
 interface CardItem {
   id: number;
@@ -7,11 +8,50 @@ interface CardItem {
 }
 interface CardProps {
   data: CardItem;
+  index: number;
+  swapIndex: (index1: number, index2: number) => void;
+}
+interface DragData {
+  id: number;
+  index: number;
 }
 
 function Card(props: CardProps) {
-  const { data } = props;
-  return <div className="card">{data.content}</div>;
+  const { data, swapIndex, index } = props;
+
+  const ref = useRef(null);
+
+  const [{ dragging }, drag] = useDrag({
+    type: "card",
+    item: {
+      id: data.id,
+      index: index,
+    },
+    collect(monitor) {
+      return {
+        dragging: monitor.isDragging(),
+      };
+    },
+  });
+
+  const [, drop] = useDrop({
+    accept: "card",
+    hover(item: DragData) {
+      swapIndex(item.index, index);
+      item.index = index;
+    },
+  });
+
+  useEffect(() => {
+    drag(ref);
+    drop(ref);
+  }, []);
+
+  return (
+    <div ref={ref} className={"card" + (dragging ? " dragging" : "")}>
+      {data.content}
+    </div>
+  );
 }
 
 function App() {
@@ -22,10 +62,23 @@ function App() {
     { id: 3, content: "Card 3" },
   ]);
 
+  const swapIndex = useCallback((index1: number, index2: number) => {
+    const tmp = cardList[index1];
+    cardList[index1] = cardList[index2];
+    cardList[index2] = tmp;
+
+    setCardList([...cardList]);
+  }, []);
+
   return (
     <div className="card-list">
-      {cardList.map((item: CardItem) => (
-        <Card data={item} key={"card_" + item.id} />
+      {cardList.map((item: CardItem, index) => (
+        <Card
+          data={item}
+          key={"card_" + item.id}
+          index={index}
+          swapIndex={swapIndex}
+        />
       ))}
     </div>
   );
