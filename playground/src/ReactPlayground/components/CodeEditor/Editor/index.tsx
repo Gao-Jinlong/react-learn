d2865c51632460c4a55e5034b3cb2a3eb06bf016
gao-jinlong <2675130594@qq.com>
2024-05-22 23:29:27 +0800

    feat:playground

--- playground/src/ReactPlayground/components/CodeEditor/Editor/index.tsx
+++ playground/src/ReactPlayground/components/CodeEditor/Editor/index.tsx
@@ -1,12 +1,25 @@
-import MonacoEditor, { OnMount } from "@monaco-editor/react";
+import MonacoEditor, { EditorProps, OnMount } from "@monaco-editor/react";
+import { createATA } from "./ata";
+import { editor } from "monaco-editor";
 
-export default function Editor() {
-  const code = `export default function App(){
-    return <div>xxx</div>
-  }`;
+export interface EditorFile {
+  name: string;
+  value: string;
+  language: string;
+}
+
+interface Props {
+  file: EditorFile;
+  onChange?: EditorProps["onChange"];
+  options?: editor.IStandaloneEditorConstructionOptions;
+}
+
+export default function Editor(props: Props) {
+  const { file, onChange, options } = props;
 
   const handleEditorMount: OnMount = (editor, monaco) => {
     // 快捷键交互
+    console.log(editor.getSupportedActions().map((action) => action.id));
     editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyJ, () => {
       editor.getAction("editor.action.formatDocument")?.run();
     });
@@ -15,15 +28,39 @@ export default function Editor() {
       jsx: monaco.languages.typescript.JsxEmit.Preserve,
       esModuleInterop: true,
     });
+
+    const ata = createATA((code, path) => {
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        code,
+        `file://${path}`
+      );
+    });
+
+    editor.onDidChangeModelContent(() => {
+      ata(editor.getValue());
+    });
   };
 
   return (
     <MonacoEditor
       height="100%"
-      path={"ginlon.tsx"}
-      language={"typescript"}
+      path={file.name}
+      language={file.language}
       onMount={handleEditorMount}
-      value={code}
+      onChange={onChange}
+      value={file.value}
+      options={{
+        fontSize: 14,
+        scrollBeyondLastLine: false,
+        minimap: {
+          enabled: false,
+        },
+        scrollbar: {
+          verticalScrollbarSize: 6,
+          horizontalScrollbarSize: 6,
+        },
+        ...options,
+      }}
     />
   );
 }
--- playground/src/ReactPlayground/components/CodeEditor/index.tsx
+++ playground/src/ReactPlayground/components/CodeEditor/index.tsx
@@ -2,10 +2,20 @@ import FileNameList from "./FileNameList";
 import Editor from "./Editor";
 
 export default function CodeEditor() {
+  const file = {
+    name: "index.tsx",
+    value: `import lodash from "lodash"; \n\nconst a = <div>ginlon</div>`,
+    language: "typescript",
+  };
+
+  function onEditorChange(...args: unknown[]) {
+    console.log(...args);
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <FileNameList />
-      <Editor />
+      <Editor file={file} onChange={onEditorChange} />
     </div>
   );
 }
