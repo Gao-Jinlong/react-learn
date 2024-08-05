553794716bb19c7fb282deb5c1fb36d862a04a6b
gao-jinlong <2675130594@qq.com>
2024-08-05 22:25:09 +0800

    feat:playground share, download

--- playground/src/ReactPlayground/utils.ts
+++ playground/src/ReactPlayground/utils.ts
@@ -1,39 +1,39 @@
-import { strFromU8, strToU8, unzlibSync, zlibSync } from "fflate"
-import { Files } from "./PlaygroundContext"
-import JSZip from "jszip"
-import { saveAs } from 'file-saver'
+import { strFromU8, strToU8, unzlibSync, zlibSync } from "fflate";
+import { Files } from "./PlaygroundContext";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
 export const fileName2Language = (name: string) => {
-    const suffix = name.split('.').pop() || ''
-    if (['js', 'jsx'].includes(suffix)) return 'javascript'
-    if (['ts', 'tsx'].includes(suffix)) return 'typescript'
-    if (['json'].includes(suffix)) return 'json'
-    if (['css'].includes(suffix)) return 'css'
-    return 'javascript'
-}
+  const suffix = name.split(".").pop() || "";
+  if (["js", "jsx"].includes(suffix)) return "javascript";
+  if (["ts", "tsx"].includes(suffix)) return "typescript";
+  if (["json"].includes(suffix)) return "json";
+  if (["css"].includes(suffix)) return "css";
+  return "javascript";
+};
 
 export function compress(data: string): string {
-    const buffer = strToU8(data)
-    const zipped = zlibSync(buffer, { level: 9 })
-    const str = strFromU8(zipped, true)
-    return btoa(str)
+  const buffer = strToU8(data);
+  const zipped = zlibSync(buffer, { level: 9 });
+  const str = strFromU8(zipped, true);
+  return btoa(str);
 }
 
 export function uncompress(base64: string): string {
-    const binary = atob(base64)
+  const binary = atob(base64);
 
-    const buffer = strToU8(binary, true)
-    const unzipped = unzlibSync(buffer)
-    return strFromU8(unzipped)
+  const buffer = strToU8(binary, true);
+  const unzipped = unzlibSync(buffer);
+  return strFromU8(unzipped);
 }
 
 export async function downloadFiles(files: Files) {
-    const zip = new JSZip()
+  const zip = new JSZip();
 
-    Object.keys(files).forEach((name) => {
-        zip.file(name, files[name].value)
-    })
+  Object.keys(files).forEach((name) => {
+    zip.file(name, files[name].value);
+  });
 
-    const blob = await zip.generateAsync({ type: 'blob' })
-    saveAs(blob, `code${Math.random().toString().slice(2, 8)}.zip`)
+  const blob = await zip.generateAsync({ type: "blob" });
+  saveAs(blob, `code${Math.random().toString().slice(2, 8)}.zip`);
 }
